037265c0738af6c42689a52065cca5d2d682da70
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-04-01 23:57:50 +0300

    Arrays CLass07 Added.

--- src/Arrays09/Class07.java
+++ src/Arrays09/Class07.java
@@ -7,7 +7,7 @@ public class Class07 {
          * write a method that accepts a 2 dimensional array and finds the max value
          * (2 boyutlu bir diziyi kabul eden ve maksimum degeri bulan bir method yazin )
          * Input : {{1,2,-3},{41,55},{-61,-17,80}}
-         * Output : 8
+         * Output : 80
          */
 
         int[][]arr={{1,2,-3},{41,55},{-61,-17,80}};
